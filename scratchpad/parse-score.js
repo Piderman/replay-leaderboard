451a59a852f43bc31f3d4d557d67250fd1125842
Matty <matty@mattycollins.com.au>
2021-06-04 20:08:53 +1000

    stream score ignores non score items

--- scratchpad/parse-score.js
+++ scratchpad/parse-score.js
@@ -1,8 +1,10 @@
-const {xml2json} = require('xml-js');
+const { xml2json } = require('xml-js');
 
 
 const xmlSnippet = `
 <Stream>
+  <Incident et="72.4">Derek Warwick(90) reported contact (0.10) with Immovable</Incident>
+  <Incident et="74.2">Chops(5) reported contact (0.03) with Immovable</Incident>
   <Score et="1596.6">Ukyo Katayama(85) lap=12 point=2 t=97.130 et=1596.547</Score>
   <Score et="1598.4">Chops(5) lap=13 point=1 t=47.108 et=1598.257</Score>
   <Score et="1607.3">Ayrton Senna(82) lap=13 point=1 t=39.390 et=1607.314</Score>
@@ -11,7 +13,7 @@ const xmlSnippet = `
 
 const driverNameDelimiter = /\(\d*\)\s/
 
-const {elements : [stream]} = JSON.parse(xml2json(xmlSnippet))
+const { elements: [stream] } = JSON.parse(xml2json(xmlSnippet))
 
 /**
  * @example
@@ -30,6 +32,10 @@ const convertScoreToObject = plainText => {
   return Object.fromEntries(timingPairs)
 }
 
+const isScore = streamElement => {
+  return streamElement?.name?.toLocaleLowerCase() === 'score'
+}
+
 /**
  * convert `<Score et="1607.3">Ayrton Senna(82) lap=13 point=1 t=39.390 et=1607.314</Score>`
  *
@@ -41,17 +47,19 @@ const convertScoreToObject = plainText => {
  *   sector: 1
  * }
  */
-const parsedStream = stream?.elements?.map(element => {
-  const [{text}] = element.elements
-  const [driverName, plainTextValues] = text.split(driverNameDelimiter)
-  const {lap, point: sector} = convertScoreToObject(plainTextValues)
+const parsedStream = stream?.elements
+  ?.filter(isScore)
+  ?.map(score => {
+    const [{ text }] = score.elements
+    const [driverName, plainTextValues] = text.split(driverNameDelimiter)
+    const { lap, point: sector } = convertScoreToObject(plainTextValues)
 
-  return {
-    driverName,
-    et: parseFloat(element.attributes.et, 10),
-    lap,
-    sector
-  }
+    return {
+      driverName,
+      et: parseFloat(score.attributes.et, 10),
+      lap,
+      sector
+    }
 })
 
-console.info(parsedStream)
+console.info({parsedStream})
